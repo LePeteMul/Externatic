a65dd20227348c421f05ea29f65f9df2978dd372
SL PRODUCTION <detections.eu@gmail.com>
2023-06-05 14:51:20 +0200

    🎉 init commit

---
+++ frontend/src/components/Elements/DashboardCard.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./DashboardCard.scss";
+
+function DashboardCard() {
+  return (
+    <div className="box">
+
+    </div>
+  )
+}
+
+export default DashboardCard;
