0a53ab7418218cd85bf65bbfdcef4330c4fd5a2a
Clément Boisrame <clement.boisrame@laposte.net>
2023-06-05 15:29:32 +0200

    CandidateCard

--- frontend/src/pages/HomePage/HomePage.jsx
+++ frontend/src/pages/HomePage/HomePage.jsx
@@ -1,9 +1,11 @@
 import React from "react";
+import CandidateCard from "../../components/Elements/CandidateCard";
 
 function HomePage() {
   return (
     <div className="HomePage">
       <h1>Hello aussi</h1>
+      <CandidateCard />
     </div>
   );
 }
