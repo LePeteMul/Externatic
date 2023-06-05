d277cc67fde36b2afca5541213369044d160fece
CLENALIO <caroline.le.nalio@gmail.com>
2023-06-05 18:57:10 +0200

    update variables boutons et input

--- frontend/src/components/Elements/BlackButton.jsx
+++ frontend/src/components/Elements/BlackButton.jsx
@@ -1,11 +1,19 @@
 import React from "react";
+import PropTypes from "prop-types";
 
-function BlackButton() {
+function BlackButton({ buttonName, buttonFunction }) {
   return (
     <div className="BlackButton">
-      <button type="button">Texte</button>
+      <button type="button" onClick={buttonFunction}>
+        {buttonName}
+      </button>
     </div>
   );
 }
+
+BlackButton.propTypes = {
+  buttonName: PropTypes.string.isRequired,
+  buttonFunction: PropTypes.string.isRequired,
+};
 
 export default BlackButton;
