86dded93e206fa96b69e92c44dc04aac93df4de6
LePeteMul <petemul80@gmail.com>
2023-06-06 14:14:21 +0200

    travail intermediaire

--- frontend/src/components/Elements/ProfilePicture.jsx
+++ frontend/src/components/Elements/ProfilePicture.jsx
@@ -1,7 +1,23 @@
 import React from "react";
+import PropTypes from "prop-types";
+// import defaultProfile from "../../assets/icons/userIcon2.png";
 
-function ProfilePicture() {
-  return <div />;
+function ProfilePicture({ profilePicture, firstname, lastname }) {
+  return (
+    <div className="ProfilePicture">
+      <img
+        className="ProfileImage"
+        src={profilePicture}
+        alt={`${firstname} ${lastname}`}
+      />
+    </div>
+  );
 }
+
+ProfilePicture.propTypes = {
+  profilePicture: PropTypes.string.isRequired,
+  firstname: PropTypes.string.isRequired,
+  lastname: PropTypes.string.isRequired,
+};
 
 export default ProfilePicture;
