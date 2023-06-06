e60a51421ec1fca624f6f0ca748c49fe2ac58bd3
YKATIA <katiayesli@yahoo.fr>
2023-06-06 10:08:30 +0200

    navbar when clicking in Menu Burger

--- frontend/src/components/Header/HeaderBasic.jsx
+++ frontend/src/components/Header/HeaderBasic.jsx
@@ -6,12 +6,18 @@ import BurgerMenu from "./BurgerMenu";
 import ProfilePicture from "../Elements/ProfilePicture";
 
 
+
 function HeaderBasic() {
+
+
+
+
   return (
     <div className="HeaderBasic">
       <BurgerMenu className="BurgerMenu" />
       <img className="Logo" src={logo} alt="Le Logo" />
       <ProfilePicture />
+      {/* <BurgerMenu  className="BurgerMenu" /> */}
     </div>
   );
 }
--- frontend/src/components/Router.jsx
+++ frontend/src/components/Router.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
-import NavBar from "./NavBar/NavBar";
+import HeaderWave from "./Header/HeaderWave";
 
 function Router() {
   return (
     <div>
       <Routes>
-        <Route path="/" element={<NavBar />} />
+        
+        <Route path="/" element={<HeaderWave/>} />
       </Routes>
     </div>
   );
