2e932f17c414df5d4f9a81859d34d5867ff1ecce
YKATIA <katiayesli@yahoo.fr>
2023-06-07 10:34:36 +0200

    registration

--- frontend/src/components/Router.jsx
+++ frontend/src/components/Router.jsx
@@ -7,21 +7,21 @@ import HeaderWave from "./Header/HeaderWave";
 import Login from "../pages/Login/Login";
 
 import JobSearch from "../pages/JobSearch/JobSearch";
-
+import Registration from "../pages/Candidate/Registration";
+import CandidateProfile from "../pages/Candidate/CandidateProfile";
 
 function Router() {
   return (
     <div>
       <Routes>
-
-      
         <Route path="/navbar" element={<NavBar />} />
         <Route path="/headerwave" element={<HeaderWave />} />
         <Route path="/login" element={<Login />} />
         <Route path="/" element={<Modele />} />
         <Route path="/home" element={<HomePage />} />
         <Route path="/jobsearch" element={<JobSearch />} />
-
+        <Route path="/registration" element={<Registration />} />
+        <Route path="/profile" element={<CandidateProfile />} />
       </Routes>
     </div>
   );
