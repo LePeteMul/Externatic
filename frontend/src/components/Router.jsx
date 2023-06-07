70343d86196c3fa7b690813713622a8408ecb224
CLENALIO <caroline.le.nalio@gmail.com>
2023-06-07 15:44:44 +0200

    details offre a finaliser

--- frontend/src/components/Router.jsx
+++ frontend/src/components/Router.jsx
@@ -5,7 +5,7 @@ import HomePage from "../pages/HomePage/HomePage";
 import NavBar from "./NavBar/NavBar";
 import HeaderWave from "./Header/HeaderWave";
 import Login from "../pages/Login/Login";
-
+import JobDetails from "../pages/JobSearch/JobDetails";
 import JobSearch from "../pages/JobSearch/JobSearch";
 
 function Router() {
@@ -18,6 +18,7 @@ function Router() {
         <Route path="/" element={<Modele />} />
         <Route path="/home" element={<HomePage />} />
         <Route path="/jobsearch" element={<JobSearch />} />
+        <Route path="/jobdetails" element={<JobDetails />} />
       </Routes>
     </div>
   );
--- frontend/src/pages/JobSearch/SearchFilters.jsx
+++ frontend/src/pages/JobSearch/SearchFilters.jsx
@@ -1,13 +1,52 @@
 import React from "react";
 import PropTypes from "prop-types";
 import BlackButton from "../../components/Elements/BlackButton";
+import InputList from "../../components/Elements/InputList";
 
 function SearchFilters({ handleClickFilters }) {
   return (
-    <div className="SearchFilters">
-      <h1>Mes filtres</h1>
-      <BlackButton buttonName="Appliquer" buttonFunction={handleClickFilters} />
-    </div>
+    <>
+      <div className="SearchFiltersTitle">
+        <h1>Mes filtres</h1>
+      </div>
+      <div className="SearchFiltersSelection">
+        <InputList
+          label="Mon rythme de travail idéal"
+          inputMessage="Selectionner le mode de télétravail"
+          data={[
+            { value: "total", name: "Total" },
+            { value: "partiel", name: "Partiel" },
+            { value: "occasionnel", name: "Occasionnel" },
+          ]}
+        />
+        <InputList
+          label="Mes technos"
+          inputMessage="Selectionner les technologies maitrisées"
+          data={[
+            { value: "JavaScript", name: "JavaScript" },
+            { value: "PHP", name: "PHP" },
+            { value: "Java", name: "Java" },
+          ]}
+        />
+        <div className="Salary">
+          <h2>Mon salaire minimum</h2>
+          <div className="AnnualSalary">
+            <p>Brut Annuel</p>
+            <p>20 000 €</p>
+          </div>
+          <div className="MensualSalary">
+            <p>Brut Mensuel</p>
+            <p>1 667 €</p>
+          </div>
+        </div>
+      </div>
+      <div className="SearchFiltersEnd">
+        <BlackButton
+          buttonName="Appliquer"
+          buttonFunction={handleClickFilters}
+        />
+      </div>
+    </>
   );
 }
 
