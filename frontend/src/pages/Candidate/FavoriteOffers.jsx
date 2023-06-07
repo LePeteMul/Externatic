009f3574e1b2230a5fdb52fd52f08791bab81017
Clément Boisrame <clement.boisrame@laposte.net>
2023-06-07 11:15:38 +0200

    1er partie

--- frontend/src/pages/Candidate/FavoriteOffers.jsx
+++ frontend/src/pages/Candidate/FavoriteOffers.jsx
@@ -1,7 +1,60 @@
 import React from "react";
+import JobCard from "../../components/Elements/JobCard";
+import HeaderBasic from "../../components/Header/HeaderBasic";
 
 function FavoriteOffers() {
-  return <div />;
+  return (
+    <div>
+      <HeaderBasic />
+      <div className="boxWithoutHeader">
+        <div className="titleFavoris">
+          <h3>Favoris</h3>
+        </div>
+        <div className="cardFavoris">
+          <JobCard
+            logo="https://www.moneyvox.fr/i/media/05l/005668l5dd.jpg"
+            companyName="Nickel"
+            job="Chef de project"
+            contractType="CDI"
+            jobCity="Nantes"
+            date="06/06/2023"
+          />
+          <JobCard
+            logo="https://www.moneyvox.fr/i/media/05l/005668l5dd.jpg"
+            companyName="Nickel"
+            job="Chef de project"
+            contractType="CDI"
+            jobCity="Nantes"
+            date="06/06/2023"
+          />
+          <JobCard
+            logo="https://www.moneyvox.fr/i/media/05l/005668l5dd.jpg"
+            companyName="Nickel"
+            job="Chef de project"
+            contractType="CDI"
+            jobCity="Nantes"
+            date="06/06/2023"
+          />
+          <JobCard
+            logo="https://www.moneyvox.fr/i/media/05l/005668l5dd.jpg"
+            companyName="Nickel"
+            job="Chef de project"
+            contractType="CDI"
+            jobCity="Nantes"
+            date="06/06/2023"
+          />{" "}
+          <JobCard
+            logo="https://www.moneyvox.fr/i/media/05l/005668l5dd.jpg"
+            companyName="Nickel"
+            job="Chef de project"
+            contractType="CDI"
+            jobCity="Nantes"
+            date="06/06/2023"
+          />
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default FavoriteOffers;
--- frontend/src/pages/Candidate/Registration.jsx
+++ frontend/src/pages/Candidate/Registration.jsx
@@ -1,7 +1,63 @@
 import React from "react";
+import InputText from "../../components/Elements/InputText";
+import BlackButton from "../../components/Elements/BlackButton";
+import HeaderBasic from "../../components/Header/HeaderBasic";
 
 function Registration() {
-  return <div />;
+  const handleClick = () => {};
+
+  return (
+    <div>
+      <HeaderBasic />
+
+      <div className="boxWithoutHeader">
+        <h2 className="info">Mon profil</h2>
+
+        <div className="civilite">
+          <h3>Genre :</h3>
+
+          <InputText />
+        </div>
+
+        <div className="name">
+          <h3>Name :</h3>
+
+          <InputText />
+        </div>
+
+        <div className="prenom">
+          <h3>Prénom :</h3>
+
+          <InputText />
+        </div>
+
+        <div className="tel">
+          <h3>Téléphone :</h3>
+
+          <InputText />
+        </div>
+
+        <div className="ville">
+          <h3>Ville :</h3>
+
+          <InputText />
+        </div>
+
+        <div className="techno">
+          <h3>Technologie maitrisées :</h3>
+
+          <InputText />
+        </div>
+
+        <div className="validation">
+          <BlackButton
+            buttonName="Valider mes changements"
+            buttonFunction={handleClick}
+          />
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Registration;
