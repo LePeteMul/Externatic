6ec67380b07eb5f22042bb1c346120e4ab944dd7
Clément Boisrame <clement.boisrame@laposte.net>
2023-06-15 09:27:44 +0200

    input cv et input image mise a jour

--- frontend/src/components/Elements/InputCv.jsx
+++ frontend/src/components/Elements/InputCv.jsx
@@ -1,55 +1,59 @@
 import React, { useState } from "react";
 import PropTypes from "prop-types";
-import uploadIcon from "../../assets/icons/upload.png";
+import upluod from "../../assets/icons/upload.png";
 
-function InputImage({ label }) {
+function InputCv({ label }) {
   const [file, setFile] = useState(null);
   const [errorMessage, setErrorMessage] = useState("");
 
   const handleFileChange = (event) => {
     const selectedFile = event.target.files[0];
     const fileSizeInBytes = selectedFile.size;
-    const maxSizeInBytes = 1 * 1024 * 1024; // 5 MB
+    const maxSizeInBytes = 5 * 1024 * 1024; // 5 MB
 
     if (fileSizeInBytes > maxSizeInBytes) {
+      // Le fichier dépasse la limite de taille
       setErrorMessage("Taille maximale dépassée (5 Mo)");
-      setFile(null);
       return;
     }
 
-    const allowedFormats = ["image/png", "image/jpeg"];
+    const allowedFormats = ["application/pdf", "image/png", "image/jpeg"];
     const fileFormat = selectedFile.type;
 
     if (!allowedFormats.includes(fileFormat)) {
+      // Le format du fichier n'est pas autorisé
       setErrorMessage(
-        "Format de fichier non autorisé. Les formats acceptés sont PNG et JPEG."
+        "Format de fichier non autorisé. Les formats acceptés sont PDF, PNG et JPEG."
       );
-      setFile(null);
       return;
     }
 
-    setFile(URL.createObjectURL(selectedFile));
-    setErrorMessage("");
+    setFile(selectedFile);
+    setErrorMessage(""); // Réinitialiser le message d'erreur
+    // Traitez le fichier ici selon vos besoins
+    console.info(selectedFile);
+    console.info(file);
   };
 
   return (
-    <div className="InputImage">
+    <div className="InputCv">
       <label htmlFor="fileInput" className="custom-label">
         {label}
-        <div className="imageHeigth">
+        <div className="input-container">
           <input
             id="fileInput"
             type="file"
-            accept=".png, .jpeg, .jpg"
+            accept=".pdf, .png, .jpeg, .jpg"
             onChange={handleFileChange}
           />
-          <div className="imageSize">
-            {file ? (
-              <img src={file} className="upload-file" alt="Uploaded Profile" />
-            ) : (
-              <img src={uploadIcon} className="upload-icon" alt="Upload" />
-            )}
+          <div className="upload-icon">
+            <img src={upluod} alt="Upload" />
             {errorMessage && <p className="error-message">{errorMessage}</p>}
+            {file && (
+              <div className="selected-file">
+                <p>{file.name}</p>
+              </div>
+            )}
           </div>
         </div>
       </label>
@@ -57,8 +61,8 @@ function InputImage({ label }) {
   );
 }
 
-InputImage.propTypes = {
+InputCv.propTypes = {
   label: PropTypes.string.isRequired,
 };
 
-export default InputImage;
+export default InputCv;
