f42de847e9921a2fb15109bdac22a8827e604ebf
Clément Boisrame <clement.boisrame@laposte.net>
2023-06-21 11:54:53 +0200

    validiation du nouveau mot de passe ajouter au profile company et candidat

--- frontend/src/pages/Candidate/CandidateProfile.jsx
+++ frontend/src/pages/Candidate/CandidateProfile.jsx
@@ -57,7 +57,14 @@ function CandidateProfile() {
         </div>
 
         <LanguageList />
-
+        <InputText
+          label="Nouveau mot de passe"
+          inputMessage="****************"
+        />
+        <InputText
+          label="Validation du nouveau mot de passe"
+          inputMessage="****************"
+        />
         <div className="validationSuppression">
           <BlackButton
             buttonName="Valider mes modifications"
