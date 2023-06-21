e2330f20883ef23fc53ec9488060b4bee3aca427
YKATIA <katiayesli@yahoo.fr>
2023-06-21 16:24:57 +0200

    Alimentation DB

--- backend/database.sql
+++ backend/database.sql
@@ -91,13 +91,16 @@ CREATE TABLE offer(
 );
 
 CREATE TABLE application(
+   id INT NOT NULL AUTO_INCREMENT,
   candidate_id INT NOT NULL,
-  application_id INT NOT NULL,
+  offer_id INT NOT NULL,
   status_id INT NOT NULL,
+  company_id INT NOT NULL,
   FOREIGN KEY(candidate_id) REFERENCES user(id),
-  FOREIGN KEY (application_id) REFERENCES offer(id),
+  FOREIGN KEY (offer_id) REFERENCES offer(id),
   FOREIGN KEY (status_id) REFERENCES status(id),
-  PRIMARY KEY (candidate_id, application_id, status_id)
+  FOREIGN KEY (company_id) REFERENCES company(id),
+  PRIMARY KEY (id)
 );
 
 
@@ -131,10 +134,58 @@ CREATE TABLE user_techno(
   PRIMARY KEY (user_id, techno_id)
 );
 
-INSERT INTO contract (contract_type) VALUES ("CDI");
+INSERT INTO contract (contract_type) VALUES ("CDI"), ("CDD"), ("Alternance"), ("Interim");
 
 
-INSERT INTO user (gender,lastname,firstname,email,phone,city,cv,admin,password,profil_picture, contact_mode,pref_job,job_city, pref_contract)VALUES ("Feminine","Dupont", "Marie", "marie.dupont@gmail.com", "06 62 19 98", "Nantes","cv", 0, "marie", "profilpic","email", "Dévloppeuse front-end","Nantes", 1);
+INSERT INTO user (gender,lastname,firstname,email,phone,city,cv,admin,password,profil_picture, contact_mode,pref_job,job_city, pref_contract)VALUES ("Female","Harley", "Guillaume", "q.harley@gmail.com", "06 62 19 98", "Nantes","queen.cv", 0, "****", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTdqUGJHg5RA-YchvuWW8TPgbvoSC2K2SiAPQ&usqp=CAU","email", "Dévloppeuse front-end","Nantes", 1),
+ ("Male","Dupont", "Eddy", "p.dupont@gmail.com", "06 82 99 98", "Bordeaux","pierre.cv", 0, "****", "https://img.freepik.com/photos-gratuite/portrait-homme-blanc-isole_53876-40306.jpg?q=10&h=200","email", "Dévloppeuse back-end","Bordeaux", 2),
+ ("Male","Martini", "Luciano", "l.martini@gmail.com", "06 82 10 91", "Nantes","luciano.cv", 0, "****", "https://images.pexels.com/photos/5378700/pexels-photo-5378700.jpeg?cs=srgb&dl=pexels-cottonbro-studio-5378700.jpg&fm=jpg","email", "Dévloppeuse back-end","Nantes", 1),
+ ("Female","Leclerc", "Amandine", "a.leclerc@gmail.com", "06 82 23 88", "Nantes","amandine.cv", 0, "****", "https://media.https://images.unsplash.com/photo-1567532939604-b6b5b0db2604?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDE0fHx8ZW58MHx8fHx8&w=1000&q=80","email", "Lead Tech Java","Nantes", 4),
+  ("Female","Chabert", "Marie", "m.chabert@gmail.com", "06 22 22 28", "Nantes","marie.cv", 0, "****", "https://www.frejoux-photographe.fr/wp-content/gallery/portrait-pro-corporate/Photographe-portrait-entreprise-pro-Corporate-toulon-Celine.jpg","email", "Dévloppeuse back-end","Nantes", 1);
 
-INSERT INTO company (company_name, email, password, phone, logo, presentation) VALUES ("Groupama", "groupama@gmail.com", "groupama", "00", "logogroupama", "We are Groupama");
+
+
+INSERT INTO company (company_name, email, password, phone, logo, presentation) VALUES ("Nickel", "recrutement@nickel.fr", "****", "01 76 49 00 00 ", "https://upload.wikimedia.org/wikipedia/fr/thumb/2/2b/Nickel-logo.svg/1200px-Nickel-logo.svg.png","service bancaire alternatif français ouvert à toute personne physique à partir de douze ans ou plus, sans condition de revenus et sans possibilité de découvert ni de crédit"),
+("allovoisins", "recrutement@allovoisins.fr", "****", "01 26 49 00 00 ", "https://upload.wikimedia.org/wikipedia/commons/thumb/3/36/Allo_voisins_icon.png/800px-Allo_voisins_icon.png","AlloVoisins est la marketplace de référence dédiée aux prestations de services et à la location de matériel , rassemblant une communauté de plus de 4 millions de membres, dont 300 000 professionnels (auto-entrepreneurs et petites entreprises)"),
+("Groupama", "recrutement@groupama.fr", "****", " 02 40 05 17 27", "https://presse.groupama.com/download-image/62432ca1728ee72abb6d0f78","Groupama est une compagnie d'assurance française qui propose une large gamme de services d'assurance et financiers"),
+("Decathlon", "recrutement@decathlon.fr", "****", "02 40 49 08 09", "https://logos-marques.com/wp-content/uploads/2020/09/Decathlon-logo.png","Entreprise française de grande distribution de sport et de loisirs, créée en 1976 par Michel Leclercq.");
+
+INSERT INTO offer (company_id, job,`date`, remote, contract_id, min_salary, max_salary, description, prerequisites, city_job, department) VALUES (1, "Data Analyst Produit","2023-06-20 15:30:45", "remote",2, 42000,45000,"Vous rejoignez l'équipe Produit et vous aidez les Product Managers à prendre des décisions clés sur leurs features en cours, ainsi qu'à construire une roadmap pertinente pour répondre aux problématiques de nos clients. Vous travaillerez à faire parler la donnée pour orienter les décisions stratégiques produit. Vous travaillerez en collaboration étroite avec les product owners pour répondre à différentes problématiques : Quels sont les comportements des clients qui favorisent l’utilisation de l’application Mobile / Espace Client Web ? Vous allez réfléchir à des solutions pour mettre en valeur les features de l’Espace client. Vous allez contribuer aux socles de données de la communauté Data. Vous allez monitorer l'activité de votre Périmètre via des Dashboards.","Esprit d'équipe","Nantes", " la Loire-Atlantique"),
+(2, "Développeur fullstack junior H/F","2023-06-20 17:30:45", "remote",1, 33000,40000," Développement front et back de nouvelles fonctionnalités pour la prise en charge à distance de patients touchés par des maladies chroniques et des fragilités sociales :
+Outils interactifs pour les visio : tableau-blanc, sondage intéractifs, etc…
+Animations in-app et outils pédagogiques pour les patients
+Fonctionnalités autour du suivi des objets connectés remis aux patients
+Tableau de bord avec des alertes pour les professionnels","Symfony & React","Nantes", " la Loire-Atlantique"),
+(3, "Lead Tech Java en DSI – H/F","2023-06-20 16:30:45", "remote",3, 10000,12000,"Vous intégrez une équipe AGILE, en tant que développeur JAVA (F/H/X). Vos missions consistent à : 
+
+Comprendre les besoins clients dans le but de définir les solutions les plus adaptées,
+Participer aux cérémonies avec les équipes PO, Scrum, MOAs, Métiers,
+Partager les bonnes pratiques de développement : qualité de code et de tests, industrialisation"," JAVA, Springboot, JS (Angular et Vue)","Nantes", " la Loire-Atlantique"),
+(4, "Développeur Back end Java H/F @Nantes gare","2023-06-21 11:30:45", "remote",2, 37000,42000,"En lien avec le directeur technique, les chefs de projets et vos collègues développeur :
+
+Analyse technique des besoins
+Architecture, conception et développement d’applications web essentiellement (plus ponctuellement il est possible d'intervenir sur des projets mobile / AR / VR / etc.)
+Tests (unitaires et fonctionnels)
+Veille technologique","Esprit d'équipe","Java", " la Loire-Atlantique"),
+(4, "Responsable de projets SI H/F","2023-06-21 10:30:45", "remote",4, 35000,40000," En tant que Responsable de Projets SI Outre-mer vous aurez en charge le pilotage des projets et le management des ressources internes MOE.
+
+En tant que Responsable de projets SI Outre-mer vous aurez pour missions :
+
+Responsable de l'équipe MOE, AMOA du périmètre SI Outre Mer (Antilles Guyane et Pacifique, Nouvelle Calédonie),
+Conduite du projet (Planning / Coûts / Délais - ~ 4m€ de budget),
+Organisation, communication, animation, management, pilotage, recrutement et suivi des prestataires","Min 3ans d'expériences","DevOps", " la Loire-Atlantique");
+
+INSERT INTO techno (techno_name) VALUES ("Java"), ("C#"), ("PHP"), ("Python"), ("React");
+
+INSERT INTO user_techno(user_id, techno_id) VALUES (1,1), (1,5),(2,3), (3,4), (4,1);
+
+INSERT INTO offer_techno(techno_id, offer_id) VALUES (4,1) ,(2,2), (5,2), (1,4);
+
+INSERT INTO favorite(candidate_id, offer_id) VALUES (1,2), (2,1), (2,3), (4,2);
+
+INSERT INTO status(status_name) VALUES ("En cours de traitement"), ("Entretien planifié"),("Accepté"),("Refusé");
+
+INSERT INTO application(candidate_id, offer_id,status_id, company_id) VALUES (1,1,1,3), (1,1,3,4), (2,2,2,1),(3,1,1,2 ),(3,5,1,3 ), (3,2,1,4), (3,3,1,1),(3,4,1,3), (4,4,1,4), (5,1,3,1), (5,4,2,1) ;
+
+
 
